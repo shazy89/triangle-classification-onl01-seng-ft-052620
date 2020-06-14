21429aa6db8e049092dd3954a8300a3e49637821
shazy89 <erdo.shazy123@gmail.com>
2020-06-14 11:17:28 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require "pry"
+
 class Triangle
   # write code here
+attr_accessor :a1, :a2, :a3
+
+
+def initialize(a1, a2, a3)
+  @a1 = a1
+  @a2 = a2 
+  @a3 = a3
+
 end
+
+def kind
+  
+  
+ if a1 + a2 <= a3 || a1 + a3 <= a2 || a2 + a3 <= a1
+    raise TriangleError
+  elsif  a1 == a2 && a2 == a3 && a1 == a3
+    :equilateral
+  elsif  a1 == a2 || a2 == a3 || a1 == a3
+    :isosceles
+  else
+    :scalene
+     end
+  end
+
+  class TriangleError < StandardError
+end
+
+
+end
+
+#binding.pry
